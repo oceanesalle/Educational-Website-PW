368db6368c277f8041c070d249795756133816c2
oceansalle <oceanesalleiae@gmail.com>
2022-06-16 14:49:07 +0200

    courses component

--- website/src/App.js
+++ website/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/common/header/Header"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home"
 import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
+        <Route exact path='/courses' component={CourseHome} />
         </Routes>
     </Router>
        
