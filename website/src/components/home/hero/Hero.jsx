36096ca30d8e7289019a24f9690008d611818919
oceansalle <oceanesalleiae@gmail.com>
2022-06-16 12:37:22 +0200

    images

--- website/src/components/home/hero/Hero.jsx
+++ website/src/components/home/hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Heading from "../../common/heading/Heading"
 import "./Hero.css"
 
 const Hero = () => {
@@ -8,7 +7,7 @@ const Hero = () => {
       <section className='hero'>
         <div className='container'>
           <div className='row'>
-            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
+
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
             <div className='button'>
               <button className='primary-btn'>
