bacf24d028eb99d464b7131ffc35374a72418300
oceansalle <oceanesalleiae@gmail.com>
2022-06-16 12:21:51 +0200

    home & hero component

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'; 
 import './App.css';
 import Header from "./components/common/header/Header"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home"
 
 function App() {
   return (
     <>
+    <Router>
         <Header />
+        <Routes>
+        <Route exact path='/' component={Home} />
+        </Routes>
+    </Router>
        
 
     </>
