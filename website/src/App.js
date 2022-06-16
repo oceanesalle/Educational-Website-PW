f12601ddb2cdeefc6f86d1afa47ab432c89b0dba
oceansalle <oceanesalleiae@gmail.com>
2022-06-16 14:21:26 +0200

    about component & css

--- website/src/App.js
+++ website/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./components/common/header/Header"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home"
+import About from "./components/about/About"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
         <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
         </Routes>
     </Router>
        
